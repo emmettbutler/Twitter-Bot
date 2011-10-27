8f0455cf57b02c9ced233bb7c1f5b40a2668be89
Emmett Butler <emmett.butler321@gmail.com>
2011-10-27 17:32:49 -0400

    usually full words only now, and a gitignore

--- scraper.py
+++ scraper.py
@@ -3,68 +3,100 @@ import re
 import random
 
 def escapes(string):
-	string = re.sub("\n", " ", string)
-	string = re.sub("&rdquo;", "", string)
-	string = re.sub("\x92", "", string)
-	string = re.sub("&nbsp;", "", string)
-	string = re.sub("Back | Next Framed", "", string)
-	string = re.sub("&quot;", "", string)
-	string = re.sub("&ldquo;", "", string)
-	string = re.sub("&rsquo;", "", string)
-	string = re.sub("&mdash;", "", string)
-	string = re.sub(r"(\xa6|\x98|\xe2|\x80|\x99|\x9d|\x9c)", "", string)
-	string = re.sub(r"<(.|\n)*?>", " ", string)
-	return string
+    string = re.sub("\n", " ", string)
+    string = re.sub("&rdquo;", "", string)
+    string = re.sub("\x92", "", string)
+    string = re.sub("&nbsp;", "", string)
+    string = re.sub("Back | Next Framed", "", string)
+    string = re.sub("&quot;", "", string)
+    string = re.sub("&ldquo;", "", string)
+    string = re.sub("&rsquo;", "", string)
+    string = re.sub("&mdash;", "", string)
+    string = re.sub(r"(\xa6|\x98|\xe2|\x80|\x99|\x9d|\x9c)", "", string)
+    string = re.sub(r"<(.|\n)*?>", " ", string)
+    return string
 
-def random_start_word(string):
-	string = string.split()
-	word = random.choice(string)
-	string = ' '.join(string)
-	index = string.index(word)
-	return string[index:]
+def random_word_range(string, length):
+    string = string.split()
+    punc_index = 0
+    for word in string:
+        for punc in (',', '.'):
+            if punc in word:
+                punc_index = string.index(word)
+    top = random.randint(0, len(string)/2)
+    if punc_index:
+        bottom = punc_index
+    else:
+        bottom = random.randint((len(string)/2)+1, len(string))
+    counter = 0
+    while len(' '.join(string[top:bottom])) > length:
+        top = random.randint(0, len(string)/2)
+        bottom = random.randint((len(string)/2)+1, len(string)-counter)
+        if counter > (len(string)/2)+2:
+            counter -= 1
+    return ' '.join(string[top:bottom])
 
-def sci_fi():
-	books = {'0671877941': 20, '067131940X': 50, '0671319760': 76, '0743499107': 6, '067187800X': 26}
-	book = random.choice(books.keys())
-	chapter = str(random.randint(1, books[book]))
-	underscore_num = 4 - len(str(chapter))
-	url = "http://www.webscription.net/10.1125/Baen/"+book+"/"+book
-	for i in range(underscore_num):
-		url += "_"
+    """
+    word = random.choice(string)
+    string = ' '.join(string)
+    index = string.index(word)
+    return string[index:]
+    """
 
-	url += chapter+".htm"
-	req = urllib2.Request(url)
-	con = urllib2.urlopen(req)
-	text = con.read()
-	paragraphs = re.findall(r"<P [^>]*>\n*.+\n*.+", text, flags=re.IGNORECASE)
+def sci_fi(short=False):
+    length = 120
+    if short:
+        length = 60
 
-	listing = []
-	for par in paragraphs:
-		listing.append(escapes(par))
+    books = {'0671877941': 20, '067131940X': 50, '0671319760': 76, '0743499107': 6, '067187800X': 26}
+    book = random.choice(books.keys())
+    chapter = str(random.randint(1, books[book]))
+    underscore_num = 4 - len(str(chapter))
+    url = "http://www.webscription.net/10.1125/Baen/"+book+"/"+book
+    for i in range(underscore_num):
+        url += "_"
 
-	l = random.choice(listing)
-	return random_start_word(l)
+    url += chapter+".htm"
+    req = urllib2.Request(url)
+    con = urllib2.urlopen(req)
+    text = con.read()
+    paragraphs = re.findall(r"<P [^>]*>\n*.+\n*.+", text, flags=re.IGNORECASE)
 
+    listing = []
+    for par in paragraphs:
+        listing.append(escapes(par))
 
-def romance():
-	books = {'55003': ['maid-for-the-billionaire', 154], '562': ['101-degrees-fahrenheit', 19], '52931': ['claimed', 96], '74690': ['kiss-on-the-bridge', 152]}
-	book = random.choice(books.keys())
-	page = str(random.randint(2, books[book][1]))
-	url = "http://www.smashwords.com/extreader/read/"+book+"/"+page+"/"+books[book][0]
-	req = urllib2.Request(url)
-	con = urllib2.urlopen(req)
-	text = con.read()
-	paragraphs = re.findall(r"<P [^>]*>\n*.+\n*.+", text, flags=re.IGNORECASE)
-
-	listing = []
-	for par in paragraphs:
-		listing.append(escapes(par))
-
-	l = random.choice(listing)
-	return random_start_word(l)
+    l = random.choice(listing)
+    return random_word_range(l, length)
 
 
-def urban_dict():
+def romance(short=False):
+    length = 120
+    if short:
+        length = 60
+
+    books = {'55003': ['maid-for-the-billionaire', 154], '562': ['101-degrees-fahrenheit', 19], '52931': ['claimed', 96], '74690': ['kiss-on-the-bridge', 152]}
+    book = random.choice(books.keys())
+    page = str(random.randint(2, books[book][1]))
+    url = "http://www.smashwords.com/extreader/read/"+book+"/"+page+"/"+books[book][0]
+    req = urllib2.Request(url)
+    con = urllib2.urlopen(req)
+    text = con.read()
+    paragraphs = re.findall(r"<P [^>]*>\n*.+\n*.+", text, flags=re.IGNORECASE)
+
+    listing = []
+    for par in paragraphs:
+        listing.append(escapes(par))
+
+    l = random.choice(listing)
+    return random_word_range(l, length)
+
+
+def urban_dict(short=False):
+    length = 120
+    if short:
+        length = 60
+
     req = urllib2.Request("http://www.urbandictionary.com/random.php", headers={'User-Agent': "Magic Browser"})
     con = urllib2.urlopen(req)
     text = con.read()
@@ -72,52 +104,48 @@ def urban_dict():
     definition = re.sub(r"<(.|\n)*?>", "", matches.group(0))
     definition = definition.replace("Urban Dictionary", "")
     definition = definition.replace("&quot;", "")
-    d = definition.split()
-    word = random.choice(d)
-    d = ' '.join(d)
-    index = d.index(word)
-    return d[index:]
+    return random_word_range(definition, length)
 
-def bash_irc():
+def bash_irc(short=False):
+    length = 120
+    if short:
+        length = 60
+
     req = urllib2.Request("http://bash.org/?random", headers={'User-Agent': "Magic Browser"})
     con = urllib2.urlopen(req)
     text = con.read()
     matches = re.search(r"<p class..qt\b[^>]*>(.+?)</p>", text)
     quote = re.sub(r"<(.|\n)*?>", "", matches.group(0))
     quote = re.sub(r"(&.*;|\"|\'|&nbsp|\(.*\)|\[.*\]|&quot;.*&quot;|&lt;.*&gt;)", "", quote)
-    q = quote.split()
-    if q:
-        w = random.choice(q)
-        q = ' '.join(q)
-        index = q.index(w)
-        return q[index:]
+    if quote:
+        return random_word_range(quote, length)
     else:
         return urban_dict()
 
 def hybrid():
-	part1 = ""
-	part2 = ""
-	upper1 = random.randint(1, 80)
-	upper2 = random.randint(1, 80)
-	chooser = random.randint(0, 3)
-	if chooser == 0:
-		part1 = urban_dict()
-	elif chooser == 1:
-		part1 = romance()
-	elif chooser == 2:
-		part2 = bash_irc()
-	else:
-		part1 = sci_fi()
-	chooser = random.randint(0, 3)
-	if chooser == 0:
-		part2 = urban_dict()
-	elif chooser == 1:
-		part2 = romance()
-	elif chooser == 2:
-		part2 = bash_irc()
-	else:
-		part2 = sci_fi()
-	return (part1[:upper1] + " " + part2[:upper2])[0:random.randint(120, 140)]
+    part1 = ""
+    part2 = ""
+    upper1 = random.randint(1, 80)
+    upper2 = random.randint(1, 80)
+    chooser = random.randint(0, 3)
+    if chooser == 0:
+        part1 = urban_dict(True)
+    elif chooser == 1:
+        part1 = romance(True)
+    elif chooser == 2:
+        part2 = bash_irc(True)
+    else:
+        part1 = sci_fi()
+    chooser = random.randint(0, 3)
+    if chooser == 0:
+        part2 = urban_dict(True)
+    elif chooser == 1:
+        part2 = romance(True)
+    elif chooser == 2:
+        part2 = bash_irc(True)
+    else:
+        part2 = sci_fi(True)
+    return (part1[:upper1] + " " + part2[:upper2])
 
 if __name__ == '__main__':
-	print hybrid()
+    print hybrid()
--- twit_bot.py
+++ twit_bot.py
@@ -24,37 +24,36 @@ def post_client():
 
     return Twitter(
         auth=OAuth(
-            oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET), 
+            oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
             secure=True, api_version='1', domain='api.twitter.com')
 
 def search_public_feed(searcher, search_string="", last_id_replied=""):
     return searcher.search(q=search_string, since_id=last_id_replied)['results']
 
 def compose_tweet(incoming=None):
-	response = "AA"
-	if incoming is not None:
-		incoming_tweet = incoming['text'].replace('@space_dad', '')
-		incoming_asker = incoming['from_user']
-		last_id_replied = str(incoming['id'])
+    response = "AA"
+    if incoming is not None:
+        incoming_tweet = incoming['text'].replace('@space_dad', '')
+        incoming_asker = incoming['from_user']
+        last_id_replied = str(incoming['id'])
 
-	make = random.randint(0, 30)
-	length = random.randint(1, 120)
-	if make < 10:
-		response = urban_dict()[0:length]
-	elif make < 13:
-		response = sci_fi()[0:length]
-	elif make < 17:
-		response = bash_irc()[0:length]
-	elif make < 20:
-		response = romance()[0:length]
-	else:
-		response = hybrid()[0:length]
+    make = random.randint(0, 30)
+    if make < 10:
+        response = urban_dict()
+    elif make < 13:
+        response = sci_fi()
+    elif make < 17:
+        response = bash_irc()
+    elif make < 25:
+        response = romance()
+    else:
+        response = hybrid()
 
-	if incoming is not None:
-		msg = '@%s %s (%s)' % (incoming_asker, response, last_id_replied[-4:])
-	else:
-		msg = '%s' % (response)
-	return msg
+    if incoming is not None:
+        msg = '@%s %s (%s)' % (incoming_asker, response, last_id_replied[-4:])
+    else:
+        msg = '%s' % (response)
+    return msg
 
 def post(poster, msg):
     if poster.statuses.update(status=msg):
@@ -62,25 +61,28 @@ def post(poster, msg):
     return False
 
 if __name__ == '__main__':
-	last_id_replied = ""
-	if len(sys.argv) > 1:
-		last_id_replied = sys.argv[1]
+    last_id_replied = ""
+    if len(sys.argv) > 1:
+        last_id_replied = sys.argv[1]
 
-	search_client = search_client()
-	post_client = post_client()
-	search_string = ""
+    search_client = search_client()
+    post_client = post_client()
+    search_string = ""
 
-	while True:
-		if search_string is not "":
-			results = search_public_feed(search_client, search_string=search_string, last_id_replied=last_id_replied)
-			for result in results:
-				tweet = compose_tweet(incoming=result)
-				print "%s\n" % (tweet)
-				if not tweet.isspace():
-					post(post_client, tweet)
-		else:
-			tweet = compose_tweet()
-			print "%s\n" % (tweet)
-			if not tweet.isspace():
-				post(post_client, tweet)
-		time.sleep(random.randint(400, 700))
+    while True:
+        try:
+            if search_string is not "":
+                results = search_public_feed(search_client, search_string=search_string, last_id_replied=last_id_replied)
+                for result in results:
+                    tweet = compose_tweet(incoming=result)
+                    print "%s\n" % (tweet)
+                    if not tweet.isspace():
+                        post(post_client, tweet)
+            else:
+                tweet = compose_tweet()
+                print "%s\n" % (tweet)
+                if not tweet.isspace():
+                    post(post_client, tweet)
+        except TwitterError:
+            continue
+        time.sleep(random.randint(400, 700))
